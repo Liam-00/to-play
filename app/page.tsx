ef2b5ae0aa14f164318c0d09948372d7b1f0bd9e
Liam <osmanliam97@gmail.com>
2023-04-30 14:28:59 +0200

    finalized initial nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Link from 'next/link'
+
 
 
 export default function Home() {
   return (
-    <>
-      <div>Hello There 1</div>
-      <Link href="./test">c l i c k here</Link>
-    </>
+    <div>
+
+    </div>
   )
 }
