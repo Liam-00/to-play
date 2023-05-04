8f7748c4c168c936cb9a323404b9f6d27c0d3b0c
Liam <osmanliam97@gmail.com>
2023-05-04 09:49:32 +0200

    mock-up of game card

--- app/components/ContentContainer/components/GameList/components/GameList__Card.tsx
+++ app/components/ContentContainer/components/GameList/components/GameList__Card.tsx
@@ -1,6 +1,6 @@
 import style from './GameList__Card.module.css'
 
-const GameList__Card = ({children, innerClass} : {children?: React.ReactElement, innerClass?: string} = {}) => {
+const GameList__Card = ({children, innerClass} : {children?: React.ReactElement[], innerClass?: string} = {}) => {
     return (
         <div className={`${style.GameList__Card_Body}`}>
             <div className={`${style.GameList__Card_Inner} ${innerClass}`}>
