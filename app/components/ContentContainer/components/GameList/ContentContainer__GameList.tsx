850fe01155f545cf12a8d675ae760928d40a254b
Liam <osmanliam97@gmail.com>
2023-05-11 18:16:08 +0200

    alter game list to take data input

--- app/components/ContentContainer/components/GameList/ContentContainer__GameList.tsx
+++ app/components/ContentContainer/components/GameList/ContentContainer__GameList.tsx
@@ -1,11 +1,21 @@
 import React from "react"
 
 import style from './ContentContainer__GameList.module.css'
+import videogame_type from "@/app/types/videogame_type"
+import GameList__GameCard from "./components/GameCard/GameList__GameCard"
 
-const ContentContainer__GameList = ({children}: {children?: React.ReactElement[]} = {children: undefined}) => {
+const ContentContainer__GameList = ({ children, gameList }: { children?: React.ReactElement | React.ReactElement[], gameList?: videogame_type[] } = { children: undefined, gameList: undefined }) => {
     return (
         <div className={`${style.ContentContainer__GameList_Body}`}>
             {children}
+            {
+                gameList ?
+                    gameList.map((val, i) => {
+                        return <GameList__GameCard videogame={val} key={i} />
+                    })
+                    :
+                    null
+            }
         </div>
     )
 }
