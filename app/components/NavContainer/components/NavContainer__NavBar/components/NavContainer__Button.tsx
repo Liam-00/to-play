d0dcc3f60fbaa7b4b8a91fe7b5d1c653d21c702c
Liam <osmanliam97@gmail.com>
2023-05-22 14:29:41 +0200

    altered nav button stylings

--- app/components/NavContainer/components/NavContainer__NavBar/components/NavContainer__Button.tsx
+++ app/components/NavContainer/components/NavContainer__NavBar/components/NavContainer__Button.tsx
@@ -1,9 +1,9 @@
 import style from './NavContainer__Button.module.css'
 
-const NavContainer__Button = ({content}: {content: string}) => {
+const NavContainer__Button = ({ content }: { content: string }) => {
     return (
         <div className={`${style.NavContainer__button_body} Text__PrimaryInterface`}>
-            <span>{content}</span>
+            <span className={`${style.NavContainer__button_text}`}>{content}</span>
         </div>
     )
 }
